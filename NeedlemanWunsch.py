3e98df1afa689a1c46dd78b63680351779c1cd65
Gahlammon <adam@dachtera.pl>
2023-11-09 14:10:55 +0100

    Clenup python code file

--- NeedlemanWunsch.py
+++ NeedlemanWunsch.py
@@ -1,4 +1,4 @@
-import numpy as np #biblioteka do macierzy
+import numpy as np
 import sys
 
 match_value = 1
@@ -11,7 +11,7 @@ def match_val(val_1, val_2):
   else:
     return mismatch_value
 
-#wczytywanie danych z pliku
+# wczytywanie danych z pliku
 if len(sys.argv) < 2:
   print("Missing input")
 else:
@@ -38,7 +38,7 @@ print('sequence 2: ', seq2)
 num1 = len(seq1)
 num2 = len(seq2)
 
-#utworzenie macierzy do zastosowania algorytmu
+# utworzenie macierzy do zastosowania algorytmu
 macierz = np.zeros((num1 + 1, num2 + 1), dtype = int)
 pochodzenie = np.zeros((num1 + 1, num2 + 1), dtype = str)
 
@@ -52,7 +52,7 @@ for i in range(1, num2 + 1):
   macierz[0][i] = licznik
   licznik = licznik - 1
 
-#uzupelnianie macierzy
+# uzupelnianie macierzy
 for x in range(1, num1 + 1):
   for y in range(1, num2 + 1):
     score = match_val(seq1[x-1], seq2[y-1])
@@ -70,13 +70,12 @@ print(macierz)
 print("direction: ")
 print(pochodzenie)
 
-#Znalezienie rozwi�zania
+# znalezienie rozwiazania
 align1 = ''
 align2 = ''
 
 while num1 > 0 and num2 > 0:
   current_score = macierz[num1][num2]
-  #print('i', i, 'j', j,  macierz[i][j], '<-current score')
   diagonal_score = macierz[num1 - 1][num2 - 1]
   up_score = macierz[num1 - 1][num2]
   left_score = macierz[num1][num2 - 1]
@@ -99,59 +98,10 @@ while num2 > 0:
   align2 += seq2[num2 - 1]
   align1 += '-'
   num2 -= 1
-
 while num1 > 0:
   align1 += seq1[num1 - 1]
   align2 += '-'
   num1 -= 1
-'''
-#do poprawy bo i, j si myl i wszytsko si pierdzieli :)
-while i > 0 and j > 0:
-
-  current_score = macierz[i][j]
-  print('i', i, 'j', j,  macierz[i][j], '<-current score')
-  diagonal_score = macierz[i-1][j-1]
-  up_score = macierz[i-1][j]
-  left_score = macierz[i][j-1]
-
-  if(current_score == diagonal_score + match_val(seq1[i-1], seq2[j-1])):
-    print(current_score, diagonal_score + match_val(seq1[i-1], seq2[j-1]), "czy wartoci si zgadzaj")
-    print("idziemy po przektnej")
-    print(seq1[i-1], "Seq1", seq2[y-1], "seq2")
-    align1 = align1 + seq1[j-1]
-    align2 = align2 + seq2[i-1]
-    print('align1, diag', align1)
-    print('align2, diag', align2)
-    j = j - 1
-    i = i - 1
-  elif current_score == up_score - 1:
-    print(current_score, up_score -1, "czy wartoci si zgadzj")
-    print("idziemy do gry")
-    print(seq1[i-1], "Seq1", seq2[y-1], "seq2")
-    align1 = align1 + seq1[j-1]
-    align2 = align2 + '-'
-    j = j - 1
-    print('align1, up', align1)
-    print('align2, up', align2)
-  elif current_score == left_score - 1:
-    print(current_score, left_score - 1, "czy wartoci si zgadzaj")
-    print("idziemy w lewo")
-    print(seq1[i-1], "Seq1", seq2[y-1], "seq2")
-    align1 = align1 + '-'
-    align2 = align2 + seq2[i-1]
-    i = i - 1
-    print('align1, left', align1)
-    print('align2, left', align2)
-
-while j > 0:
-  align1 += seq1[j-1]
-  align2 += '-'
-  j -= 1
-while i > 0:
-  align1 += '-'
-  align2 += seq2[i-1]
-  i -= 1
-'''
 
 print('align 1: ', align1[::-1])
 print('align 2: ', align2[::-1])
